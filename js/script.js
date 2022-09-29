d42fa76ff532d5949505c5002d034736cb01cb17
TaricOv <tareq.ouf@gmail.com>
2022-09-29 04:17:55 +0200

    added highligh to tabs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 let allListItems = Array.from(document.querySelectorAll(".list__item"))
 
 Array.from(document.querySelectorAll(".used__langs span")).map(ele => {
+
     ele.addEventListener("click", function () {
 
+        Array.from(document.querySelectorAll(".used__langs span")).filter(v => (v !== ele)).map(vv => vv.classList.remove("active__filter"))
+
+        ele.classList.add("active__filter")
         let filtered = ele.dataset.filter
 
         allListItems.map(v => v.classList.remove("hide__item"))
